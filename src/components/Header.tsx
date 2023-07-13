276c63f5ff640a5949048f2fd9e42c448778a63f
kelly <kelly@kelly-X470-AORUS-ULTRA-GAMING.home>
2023-07-13 15:27:19 +0200

    Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,31 @@
 import React from 'react'
 
+// icons
+import { GiHamburgerMenu } from 'react-icons/gi'
+import { HiSearch } from 'react-icons/hi'
+
 export default function Header(): JSX.Element {
   return (
-    <div className="bg-mantle">
-      <h1 className="text-primary">Todo App</h1>
+    <div className="flex justify-between items-center px-4 py-4 text-xl lg:p-6 bg-mantle text-text">
+      <div className="flex gap-3 items-center">
+        <button className="text-xl 2xl:hidden">
+          <GiHamburgerMenu />
+        </button>
+        <h1 className="text-xl font-bold text-primary">Todo App</h1>
+      </div>
+      <div className="flex gap-3 items-center">
+        <input
+          className="hidden px-3 py-1 rounded border text-text border-secondary lg:block bg-base"
+          type="text"
+          placeholder="search"
+        />
+        <button className="lg:hidden">
+          <HiSearch />
+        </button>
+        <button className="px-3 py-1 text-base rounded bg-primary">
+          add task
+        </button>
+      </div>
     </div>
   )
 }
